a3db1990d5d54b706ea7e1801af5a80600ddd639
katukojwala Sreemaye <Sirisreemaye@gmail.com>
2026-06-11 23:04:18 +0530

    Update login API endpoint to production URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login({ setToken, darkMode }) {
     e.preventDefault();
     setError('');
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { username, password });
+      const res = await axios.post(' https://stock-trade-simulator.onrender.com/api/auth/login', { username, password });
       setToken(res.data.token);
       navigate('/dashboard');
     } catch (err) {
